27af1f7581a900ea8795e6340698b9fa89c2f95c
rhayven <rhayvenrojas10@gmail.com>
2026-07-23 23:03:08 +0800

    YOOHOOOO

--- js/add-memory.js
+++ js/add-memory.js
@@ -4,9 +4,9 @@ const preview = document.getElementById("preview");
 imageInput.addEventListener("change", function () {
   const file = imageInput.files[0];
   if (file) {
-    preview.src = URL.createObjectURL(file);
-    preview.hidden = false;
-
-    uploadLabel.style.display = "none";
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+    img.classList.add("preview-image");
+    gallery.appendChild(img);
   }
 });
